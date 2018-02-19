246b062cfcdeaab09f637779d85b3df9fb8b709c
Vlad_Hmara <hmara_vlad@mail.ru>
2018-02-19 18:55:56 +0200

    First commit: add textures, class Bird, Tree, and some logic

---
+++ FlappyBird/Bird.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows.Threading;
+using System.Windows;
+using System.Windows.Interop;
+using System.IO;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace FlappyBird
+{
+    class Bird
+    {
+        static Bitmap[] birdImgs = new Bitmap[20];
+
+        Bitmap[] birdImg = new Bitmap[2];
+        PictureBox pictureBox;
+        private Thread thread; 
+
+        public enum Color
+        {
+            Yellow,
+            Red,
+            Blue,
+            Green,
+            White,
+            Black,
+            Brown,
+            Purple,
+            Rose,
+            Orange
+        }
+
+
+        public Bird(Form form, Color c)
+        {
+            birdImg[0] = birdImgs[(int)c * 2];
+            birdImg[1] = birdImgs[(int)c * 2 + 1];
+
+            pictureBox = new PictureBox();
+            pictureBox.Location = new System.Drawing.Point(0,0);
+            pictureBox.SizeMode = PictureBoxSizeMode.AutoSize;
+            form.Controls.Add(pictureBox);
+            pictureBox.Image = birdImg[0];
+        }
+
+        static Bird()
+        {
+            birdImgs = SplitImage(new Bitmap(@"D:\Education 2018\FlappyBird\FlappyBird\Textures\img_bird.png"), 2, 10).ToArray();
+        }
+
+        public void WagStart()
+        {
+            thread = new Thread(new ThreadStart(()=> {
+                int num = 1;
+                while (true)
+                {
+                    num = num == 0 ? 1 : 0;
+                    pictureBox.BeginInvoke((MethodInvoker)(() =>
+                    {
+                        pictureBox.Image = birdImg[num];
+                    }));
+                    Thread.Sleep(200);
+                }
+            }));
+            thread.Start();
+        }
+        public void WagStop()
+        {
+            thread.Abort();
+        }
+        public static List<Bitmap> SplitImage(Bitmap img, int NumX, int NumY)
+        {
+            List<Bitmap> listBmp = new List<Bitmap>();
+
+            Bitmap bmp = new Bitmap(img);
+            int width = img.Width / NumX;
+            int height = img.Height / NumY;
+
+            for (int i = 0; i < NumY; i++)
+                for (int j = 0; j < NumX; j++)
+                {
+                    Rectangle rect = new Rectangle(j * width, i * height, width, height);
+                    Bitmap region = new Bitmap(rect.Width, rect.Height);
+                    using (Graphics g = Graphics.FromImage(region))
+                    {
+                        g.DrawImage(bmp, 0, 0, rect, GraphicsUnit.Pixel);
+                    }
+                    listBmp.Add(region);
+                }
+            return listBmp;
+        }
+    }
+}
---
+++ FlappyBird/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace FlappyBird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(751, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(727, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 329);
+            this.listBox1.TabIndex = 2;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::FlappyBird.Properties.Resources.img_ground;
+            this.pictureBox4.Location = new System.Drawing.Point(66, 399);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(537, 100);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox4.TabIndex = 0;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1191, 519);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
