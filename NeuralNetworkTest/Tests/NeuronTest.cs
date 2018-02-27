e2412e9f1816b5497cf2c89d15cc9e5f32e22c14
Vlad_Hmara <hmara_vlad@mail.ru>
2018-02-27 20:26:18 +0200

    Add few test for Neuron class

--- NeuralNetworkTest/Tests/NeuronTest.cs
+++ NeuralNetworkTest/Tests/NeuronTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NeuralNetworkClasses.Classes;
 
 namespace NeuralNetworkTest.Tests
 {
@@ -7,8 +8,42 @@ namespace NeuralNetworkTest.Tests
     public class NeuronTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void NeuronActivationTest1()
         {
+            // arrange 
+            Neuron neuron = new Neuron();
+            Sinaps sinaps1 = new Sinaps(new Neuron() { Data = 0.5 }, 0.2);
+            Sinaps sinaps2 = new Sinaps(new Neuron() { Data = 0.3 }, 0.4);
+            neuron.Sinapses.Add(sinaps1);
+            neuron.Sinapses.Add(sinaps2);
+            double expected = 0;
+            foreach (Sinaps sinaps in neuron.Sinapses)
+                expected += sinaps.GetData();
+            expected = (Math.Exp(2 * expected) - 1) / (Math.Exp(2 * expected) + 1);
+
+            // act
+            neuron.Activation();
+
+            // assert  
+            double actual = neuron.Data;
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void NeuronActivationTest2()
+        {
+            // arrange 
+            Neuron neuron = new Neuron();
+            Sinaps sinaps1 = new Sinaps(new Neuron() { Data = 0.5 }, 0.2);
+            Sinaps sinaps2 = new Sinaps(new Neuron() { Data = 0.3 }, 0.4);
+            neuron.Sinapses.Add(sinaps1);
+            neuron.Sinapses.Add(sinaps2);
+
+            // act
+            neuron.Activation();
+
+            // assert  
+            double actual = neuron.Data;
+            Assert.IsTrue(actual >=- 1 && actual <= 1);
         }
     }
 }
