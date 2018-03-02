8f97f7953ed5e554d3309927a0e12afe19d1c51f
Alexander <alex.gavriliak@gmail.com>
2018-03-03 01:19:45 +0200

    Added Mutation

--- NeuralNetworkClasses/Classes/GeneticAlgorithm.cs
+++ NeuralNetworkClasses/Classes/GeneticAlgorithm.cs
@@ -6,10 +6,22 @@ namespace NeuralNetworkClasses.Classes
 {
     public static class GeneticAlgorithm
     {
+        static private Random r = new Random();
         public static void Evolution(ref List<IGenetic> items)
         {
             Selection(ref items);
-            items[items.Count / 3 + 1].NeuralNetworkItem = Crossing(items[0].NeuralNetworkItem, items[1].NeuralNetworkItem);
+
+            for (int i = items.Count / 3 + 1; i < items.Count; i++)
+            {
+                if (r.Next(0, 1) == 0)
+                    items[i].NeuralNetworkItem = Crossing(items[0].NeuralNetworkItem, items[1].NeuralNetworkItem);
+                else
+                    items[i].NeuralNetworkItem = Crossing(items[2].NeuralNetworkItem, items[3].NeuralNetworkItem);
+
+            }
+
+            Mutation(ref items);
+
 
         }
 
@@ -25,22 +37,23 @@ namespace NeuralNetworkClasses.Classes
             List<double> parent1Genome = parent1.GetGenome();
             List<double> parent2Genome = parent2.GetGenome();
             int size = parent1Genome.Count;
-            int CurrentIndex = 0;
 
-            for (int i = 8; i < size; i++)
+            int randomIndex = r.Next(0, parent1Genome.Count - 1);
+            int currentIndex = 0;
+            for (int i = randomIndex; i < size; i++)
             {
-                for (int j = 0; j < 8; j++)
+                for (int j = 0; j < randomIndex; j++)
                 {
-                    if (Math.Abs(parent2Genome[CurrentIndex] - parent1Genome[j] - 0.001) <= 0.02)
+                    if (Math.Abs(parent2Genome[currentIndex] - parent1Genome[j] - 0.001) <= 0.02)
                     {
-                        if (++CurrentIndex >= size)
+                        if (++currentIndex >= size)
                             break;
                         j = -1;
                     }
                 }
-                if (CurrentIndex >= size)
+                if (currentIndex >= size)
                     break;
-                parent1Genome[i] = parent2Genome[CurrentIndex++];
+                parent1Genome[i] = parent2Genome[currentIndex++];
             }
 
             parent1.SetGenome(parent1Genome);
@@ -48,8 +61,30 @@ namespace NeuralNetworkClasses.Classes
             return parent1;
         }
 
-        private static void Mutation()
+        private static void Mutation(ref List<IGenetic> items)
         {
+
+            //Коефициент мутирующих особей
+            double kMutation = 0.1;
+            //Коефициент приращивания
+            double kAdd = r.NextDouble() * 0.1;
+
+            //Всего будет мутированных особей
+            for (int i = 0; i < items.Count * kMutation; i++)
+            {
+                int current = r.Next(0, items.Count - 1);
+
+                List<double> genome = items[current].NeuralNetworkItem.GetGenome();
+                int curerntGenome = r.Next(0, genome.Count - 1);
+
+                if (r.Next(0, 1) == 0)
+                    genome[curerntGenome] += genome[curerntGenome] + kAdd <= 1 ? kAdd : -1 * kAdd;
+                else
+                    genome[curerntGenome] -= genome[curerntGenome] - kAdd >= -1 ? kAdd : -1 * kAdd;
+
+                items[current].NeuralNetworkItem.SetGenome(genome);
+
+            }
 
         }
     }
